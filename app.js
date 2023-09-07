ef95e90c7933beb781c15964bd7081037169fedf
manichandmudedla <mudedlamanichand030102@gmail.com>
2023-09-07 19:24:46 +0530

    proxy server

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // Choose a port of your choice
+const axios=require('axios')
+// Middleware to enable CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Replace '*' with your client's domain for security
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+// Define a route for proxying API requests
+app.get('/', (req, res) => {
+  axios.get('https://newsapi.org/v2/everything?q=india&language=en&sortBy=publishedAt&apiKey=2abe11e647cd45f0957adc6e0f2db6e4')
+    .then(response => res.json(response.data))
+    .catch(error => res.status(500).json({ error: 'Proxy error' }));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
